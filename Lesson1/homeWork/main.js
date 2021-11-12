a1a67c6a54c2cf798052aa139a1c6ab20a843e53
Nazar-Sergeev <privat.nazar@gmail.com>
2021-11-12 22:48:46 +0200

    Initial commit

---
+++ Lesson1/homeWork/main.js
@@ -0,0 +1,146 @@
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let j = 3.14;
+// let k = 2.7;
+// let bool1 = true;
+// let bool2 = false;
+
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(j);
+// console.log(k);
+// console.log(bool1);
+// console.log(bool2);
+//
+// document.write(a + '</br>');
+// document.write(b + '</br>');
+// document.write(c + '</br>');
+// document.write(d + '</br>');
+// document.write(e + '</br>');
+// document.write(f + '</br>');
+// document.write(g + '</br>');
+// document.write(h + '</br>');
+// document.write(j + '</br>');
+// document.write(k + '</br>');
+// document.write(bool1 + '</br>');
+// document.write(bool2 + '</br>');
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(h);
+// alert(j);
+// alert(bool1);
+// alert(bool2);
+
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+//
+// let firstName = 'Nazar';
+// let middleName = 'Yurievich';
+// let lastName = 'Sergeev';
+//
+// let person = `${firstName} ${middleName} ${lastName}`;
+//
+// console.log(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// let firstName = prompt('What`s your name????');
+// let middleName = prompt('What`s your middle name????');
+// let lastName = prompt('What`s your last name????');
+//
+// console.log(firstName);
+// console.log(middleName);
+// console.log(lastName);
+
+// Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+// let age = prompt('How old are you?');
+//
+// console.log(age);
+//
+// alert(`Hey, ${firstName} ${middleName} you are ${age} years old`)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//
+//     let a = 100;
+//     let b = '100';
+//     let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+// console.log(5 < 6) //-> true
+// console.log(5 > 6) //-> false
+// console.log(5 >= 6) //-> false
+// console.log(5 == 6) //-> false
+// console.log(10 <= 10); //-> true
+// console.log(10 >= 10);//-> true
+// console.log(10 !== 10); //-> false
+// console.log(10 != 10) //-> false
+// console.log(10 < 10); //-> false
+// console.log(123 === '123'); //-> false
+// console.log(123 == '123') //-> true
+
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//
+//     let str = "20";
+//     let a = 5;
+//     document.write(str + a + "<br/>");
+//     document.write(str - a + "<br/>");
+//     document.write(str * "2" + "<br/>");
+//     document.write(str / 2 + "<br/>");
+
+
+
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+
+let numbers = [1, 3, 7, 4, 8, 9, 2, 12, 15, 17];
+
+let result = numbers[0] + numbers[1] +  numbers[2] +  numbers[4] +  numbers[4] +  numbers[5] +  numbers[6] +  numbers[7] +  numbers[8] +  numbers[9];
+console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+let book = {
+    name: 'Harry Potter',
+    number: 245,
+    genre: 'fantastic'
+}
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// 4. Створити масив об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
