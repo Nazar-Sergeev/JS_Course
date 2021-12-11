e8deedfb20bc3a8af24249ce781c41126b02ec7e
Nazar-Sergeev <privat.nazar@gmail.com>
2021-12-12 00:35:21 +0200

    homeWork

--- Lesson12/cw/cw.js
+++ Lesson12/cw/cw.js
@@ -2,3 +2,9 @@
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 // https://jsonplaceholder.typicode.com/posts
 //     зробити кнопку до кожного поста. при кліку на яку виводяться в окремий блок всі коментарі поточного поста
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(value => value.json())
+    .then(response => {
+    
+    });
---
+++ Lesson12/hw/hw.js
@@ -0,0 +1,38 @@
+
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ об'єкти. Застилізувати, за допомоги css, щоб отримати 5 елементів в рядку.
+// Для кожного елементу свій блок div.post
+// Всі характеристики повинні мати свої блоки всередені div.post
+// https://jsonplaceholder.typicode.com/posts
+//
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(value => value.json())
+//     .then(response => {
+//         for (const post of response) {
+//             let divPost = document.createElement('div');
+//             divPost.classList.add('post');
+//             divPost.innerHTML = `<h3>${post.id}</h3> <h2>${post.title}</h2> <p>${post.body}</p>`
+//
+//             div.appendChild(divPost);
+//             document.body.appendChild(div);
+//         }
+//     });
+
+
+// Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті.
+//     Для кожного елементу свій блок div.comment
+// Всі характеристики повинні мати свої блоки всередені div.comment
+// https://jsonplaceholder.typicode.com/comments
+//
+// fetch('https://jsonplaceholder.typicode.com/comments')
+//     .then(value => value.json())
+//     .then(response => {
+//         for (const comment of response) {
+//             let divComment = document.createElement('div');
+//             divComment.classList.add('comment');
+//             divComment.innerHTML = `<h3>${comment.id}</h3> <h2>${comment.name}</h2> <h4>${comment.email}</h4> <p>${comment.body}</p>`
+//
+//             document.body.appendChild(divComment);
+//         }
+//
+//
+//     });
