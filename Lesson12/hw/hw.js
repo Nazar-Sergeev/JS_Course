5072894744789e7ae449902815d2d8de1e308ea9
Nazar-Sergeev <privat.nazar@gmail.com>
2021-12-12 17:43:57 +0200

    homeWork

--- Lesson12/hw/hw.js
+++ Lesson12/hw/hw.js
@@ -1,12 +1,14 @@
-
+//
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ об'єкти. Застилізувати, за допомоги css, щоб отримати 5 елементів в рядку.
 // Для кожного елементу свій блок div.post
 // Всі характеристики повинні мати свої блоки всередені div.post
 // https://jsonplaceholder.typicode.com/posts
-//
+
 // fetch('https://jsonplaceholder.typicode.com/posts')
 //     .then(value => value.json())
 //     .then(response => {
+//             let div = document.createElement('div');
+//             div.classList.add('wrap');
 //         for (const post of response) {
 //             let divPost = document.createElement('div');
 //             divPost.classList.add('post');
@@ -22,17 +24,19 @@
 //     Для кожного елементу свій блок div.comment
 // Всі характеристики повинні мати свої блоки всередені div.comment
 // https://jsonplaceholder.typicode.com/comments
-//
+
 // fetch('https://jsonplaceholder.typicode.com/comments')
 //     .then(value => value.json())
 //     .then(response => {
+//         let div = document.createElement('div');
+//             div.classList.add('wrap');
 //         for (const comment of response) {
 //             let divComment = document.createElement('div');
 //             divComment.classList.add('comment');
 //             divComment.innerHTML = `<h3>${comment.id}</h3> <h2>${comment.name}</h2> <h4>${comment.email}</h4> <p>${comment.body}</p>`
 //
-//             document.body.appendChild(divComment);
+//             div.appendChild(divComment)
+//             document.body.appendChild(div);
 //         }
-//
 //
 //     });
