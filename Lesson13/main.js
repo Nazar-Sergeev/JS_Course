26cab01040f7d4c85e8dc2083139f38fee503290
Nazar-Sergeev <privat.nazar@gmail.com>
2021-12-26 00:28:36 +0200

    added async await and callbacks

--- Lesson13/main.js
+++ Lesson13/main.js
@@ -11,109 +11,231 @@
 
 
 
-function wakeUp(wokeUp) {
-        return new Promise((resolve, reject) => {
-    setTimeout(() => {
-            if (wokeUp) {
-                resolve('Good Morning');
-            }
-            reject('Go sleep');
-        }, 300);
-    });
-}
+// function wakeUp(wokeUp) {
+//         return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//             if (wokeUp) {
+//                 resolve('Good Morning');
+//             }
+//             reject('Go sleep');
+//         }, 300);
+//     });
+// }
+//
+//
+// function breakfast(food) {
+//         return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//             if (food) {
+//                 resolve(`Drink ${food}`)
+//             }
+//             reject('No eat');
+//         }, 1000);
+//
+//     });
+// }
+//
+// function goShower(shower) {
+//     return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (shower) {
+//             resolve('Yuhu hu hu cool shower')
+//         }
+//         reject('Foooo');
+//     }, 500);
+//
+//     });
+// }
+//
+// function goWork(isJobDone) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (isJobDone) {
+//                 resolve('$$$$')
+//             }
+//             reject('000 money (((')
+//         }, 3000);
+//     });
+// }
+//
+// function dinner(food) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//         if (food) {
+//             resolve( `Nyam nyam ${food}`)
+//         }
+//         reject('(((((')
+//
+//
+//         }, 1000);
+//     });
+// }
+//
+// function goHome(home) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//         if (home) {
+//             resolve('Hooray i`m home')
+//         }
+//         reject('still working')
+//
+//         }, 100);
+//     });
+// }
 
 
-function breakfast(food) {
-        return new Promise((resolve, reject) => {
-    setTimeout(() => {
-            if (food) {
-                resolve(`Drink ${food}`)
-            }
-            reject('No eat');
-        }, 1000);
-
-    });
-}
-
-function goShower(shower) {
-    return new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if (shower) {
-            resolve('Yuhu hu hu cool shower')
-        }
-        reject('Foooo');
-    }, 500);
-
-    });
-}
-
-function goWork(isJobDone) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (isJobDone) {
-                resolve('$$$$')
-            }
-            reject('000 money (((')
-        }, 3000);
-    });
-}
-
-function dinner(food) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        if (food) {
-            resolve( `Nyam nyam ${food}`)
-        }
-        reject('(((((')
+// wakeUp(true)
+//     .then(morning => {
+//         console.log(morning);
+//
+//     return breakfast('coffe')
+//     })
+//     .then(eat =>{
+//         console.log(eat);
+//     return goShower(true)
+//     })
+//     .then(yeas => {
+//          console.log(yeas);
+//
+//     return goWork(true)
+//     })
+//     .then(completed =>{
+//         console.log(completed);
+//
+//     return dinner('steak and potatoes')
+//     })
+//     .then(eat =>{
+//        console.log(eat)
+//
+//     return goHome(true);
+//     })
+//
+//     .then(myHouse =>{
+//         console.log(myHouse)
+//     })
 
 
-        }, 1000);
-    });
-}
-
-function goHome(home) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        if (home) {
-            resolve('Hooray i`m home')
-        }
-        reject('still working')
-            
-        }, 100);
-    });
-}
+// async function schedule() {
+//     console.log(await wakeUp(true));
+//     console.log(await breakfast('tea'));
+//     console.log(await goShower(true));
+//     console.log(await goWork(true));
+//     console.log(await dinner('Grechka'));
+//     console.log(await goHome(true));
+// }
+//
+// schedule();
 
 
-
-wakeUp(true)
-    .then(morning => {
-        console.log(morning);
-    
-    return breakfast('coffe')
-    })
-    .then(eat =>{
-        console.log(eat);
-    return goShower(true)
-    })
-    .then(yeas => {
-         console.log(yeas);
-
-    return goWork(true)
-    })
-    .then(completed =>{
-        console.log(completed);
-
-    return dinner('steak and potatoes')
-    })
-    .then(eat =>{
-       console.log(eat)
-
-    return goHome(true);
-    })
-
-    .then(myHouse =>{
-        console.log(myHouse)
-    })
-
-
-
+//
+// function wakeUp(wokeUp, cb) {
+//         setTimeout(() => {
+//             if (wokeUp) {
+//                 cb(null, 'Good Morning');
+//             } else {
+//             cb('Go sleep');
+//             }
+//         }, 300);
+// }
+//
+//
+// function breakfast(food, cb) {
+//         setTimeout(() => {
+//             if (food) {
+//                 cb(null, `Drink ${food}`);
+//             } else {
+//                 cb('No eat');
+//             }
+//         }, 1000);
+//
+// }
+//
+// function goShower(shower, cb) {
+//         setTimeout(() => {
+//             if (shower) {
+//                 cb(null, 'Yuhu hu hu cool shower');
+//             } else {
+//                 cb('Foooo');
+//             }
+//         }, 500);
+// }
+//
+// function goWork(isJobDone, cb) {
+//         setTimeout(() => {
+//             if (isJobDone) {
+//                 cb(null, '$$$$');
+//             } else {
+//                 cb('000 money (((');
+//             }
+//         }, 3000);
+// }
+//
+// function dinner(food, cb) {
+//         setTimeout(() => {
+//             if (food) {
+//                 cb(null, `Nyam nyam ${food}`);
+//             } else {
+//                 cb('(((((');
+//             }
+//
+//         }, 1000);
+// }
+//
+// function goHome(home, cb) {
+//         setTimeout(() => {
+//             if (home) {
+//                 cb(null, 'Hooray i`m home');
+//             } else {
+//                 cb('still working');
+//             }
+//
+//         }, 100);
+// }
+//
+// wakeUp(true, (err, morning) => {
+//     if (err) {
+//         console.log(err);
+//         return
+//     }
+//     console.log(morning);
+//
+//     breakfast('tea', (err, eat) => {
+//         if (err) {
+//             console.log(err)
+//             return
+//         }
+//     console.log(eat)
+//
+//         goShower(true, (err, yeas) => {
+//         if (err) {
+//             console.log(err);
+//             return
+//         }
+//         console.log(yeas)
+//
+//             goWork(true, (err, completedWork) => {
+//             if (err) {
+//                 console.log(err)
+//                 return
+//             }
+//             console.log(completedWork);
+//
+//                 dinner('potato', (err, eat) => {
+//                     if (err) {
+//                         console.log(err);
+//                         return;
+//                     }
+//                     console.log(eat);
+//
+//                     goHome(true, (err, home) => {
+//                         if (err) {
+//                             console.log(err);
+//                             return;
+//                         }
+//                         console.log(home);
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
